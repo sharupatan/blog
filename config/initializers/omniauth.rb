e978b7403103a085048e18e69c8748ac2f1460ab
poojapatidarweb <147521890+poojapatidarweb@users.noreply.github.com>
2024-03-15 22:43:50 +0300

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '1001749229758-dm6m952ghopiqptja99l518hsc4strfa.apps.googleusercontent.com',
+  provider :google_oauth2, '*************strfa.apps.googleusercontent.com',
                            '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',
                            {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
